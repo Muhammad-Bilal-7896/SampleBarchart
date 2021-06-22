5c88de8fbfc37fc76eb79fb0ffc4c65c371b5dd8
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-06-22 17:55:28 +0500

    done with frequency chart

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import store from './store'
 
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
+      <App />,
   document.getElementById('root')
 );
 
